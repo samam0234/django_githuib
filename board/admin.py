e365f0211b4323e2127c4a90486fbbdf09243fa3
while <limjinheng0120@gmail.com>
2026-03-16 16:33:59 +0900

    feat : 어드민 모델 기능 추가

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ['title', 'created_at', 'updated_at']
+    search_fields = ['title', 'content']
+    list_filter = ['created_at']
+
 # 여기에 관리자 페이지에 표시할 모델을 등록합니다.
-admin.site.register(Post)   # Admin페이지에서 Post 모델을 관리하도록 등록
+admin.site.register(Post, PostAdmin)   # Admin페이지에서 Post 모델을 관리하도록 등록
 
